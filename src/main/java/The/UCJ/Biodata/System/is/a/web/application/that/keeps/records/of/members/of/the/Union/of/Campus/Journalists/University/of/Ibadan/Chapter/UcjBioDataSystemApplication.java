ce34b70e219cd5ca5e41e14baab3b06ae468eb97
intellisense <oyeknamiakandeworld@gmail.com>
2020-03-22 07:36:16 +0100

    Inital commit

---
+++ src/main/java/The/UCJ/Biodata/System/is/a/web/application/that/keeps/records/of/members/of/the/Union/of/Campus/Journalists/University/of/Ibadan/Chapter/UcjBioDataSystemApplication.java
@@ -0,0 +1,13 @@
+package The.UCJ.Biodata.System.is.a.web.application.that.keeps.records.of.members.of.the.Union.of.Campus.Journalists.University.of.Ibadan.Chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcjBioDataSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcjBioDataSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/The/UCJ/Biodata/System/is/a/web/application/that/keeps/records/of/members/of/the/Union/of/Campus/Journalists/University/of/Ibadan/Chapter/UcjBioDataSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package The.UCJ.Biodata.System.is.a.web.application.that.keeps.records.of.members.of.the.Union.of.Campus.Journalists.University.of.Ibadan.Chapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcjBioDataSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
